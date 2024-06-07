2b944892012d68764d48cd8e6a903c6ec3712df6
Rendyuiop <110321149+Rendyuiop@users.noreply.github.com>
2024-06-08 04:14:56 +0700

    Create routes.js

---
+++ Tugas2/src\TBDGRB/routes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const controller = require("./controller");
+
+const router = Router();
+
+// Routes to get reviews
+router.get("/reviews", controller.getAllReviews);
+router.get("/reviews/:id", controller.getReviewByID);
+router.get("/reviews/user/:userid", controller.getReviewsByUser);
+router.get("/reviews/book/:bookid", controller.getReviewsByBook);
+
+// Routes to create a new review
+router.post("/reviews", controller.addReview);
+
+// Routes to update a review
+router.put("/reviews/:id", controller.updateReview);
+
+// Routes to delete a review
+router.delete("/reviews/:id", controller.deleteReview);
+
+module.exports = router;
