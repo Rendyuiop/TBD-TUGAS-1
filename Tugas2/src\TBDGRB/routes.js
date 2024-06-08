3e7619deb84aa4deff852fbe2da9ca73415d18a3
Rendyuiop <110321149+Rendyuiop@users.noreply.github.com>
2024-06-08 10:28:16 +0700

    Update routes.js

--- Tugas2/src\TBDGRB/routes.js
+++ Tugas2/src\TBDGRB/routes.js
@@ -3,17 +3,18 @@ const controller = require("./controller");
 
 const router = Router();
 
+// Routes to create a new review,
+router.post("/reviews", controller.addReview);
+
+// Routes to update a review
+router.put("/reviews/:id", controller.updateReview);
+
 // Routes to get reviews
 router.get("/reviews", controller.getAllReviews);
 router.get("/reviews/:id", controller.getReviewByID);
 router.get("/reviews/user/:userid", controller.getReviewsByUser);
 router.get("/reviews/book/:bookid", controller.getReviewsByBook);
-
-// Routes to create a new review
-router.post("/reviews", controller.addReview);
-
-// Routes to update a review
-router.put("/reviews/:id", controller.updateReview);
+router.get("/reviews/rate/:rating", controller.getReviewsByRate);
 
 // Routes to delete a review
 router.delete("/reviews/:id", controller.deleteReview);
