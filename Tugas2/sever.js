c395594a5227582962512c8ce558a67bd4c3438c
Rendyuiop <110321149+Rendyuiop@users.noreply.github.com>
2024-06-08 14:01:38 +0700

    Update sever.js

--- Tugas2/sever.js
+++ Tugas2/sever.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const TBDGRBRoutes = require("./src/TBDGRB/routes");
 
-
 const app = express();
-const port = 3000;
+const port = 3500;
 
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("Good Reading Bookstore");
 })
