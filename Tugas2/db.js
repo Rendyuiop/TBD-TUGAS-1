243fe7e26ce80218727cc3db643e7f9da2237f99
Rendyuiop <110321149+Rendyuiop@users.noreply.github.com>
2024-06-08 14:02:56 +0700

    Update db.js

--- Tugas2/db.js
+++ Tugas2/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    database: "Tugas1",
+    database: "GRBTBD",
     password: "487733",
     port: "5432",
 });
